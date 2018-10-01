bb592f69746f42c7f34142ae7136ef23d5450085
Hugo Crampont <hugo.crampont@altares.com>
2018-10-01 13:35:12 +0200

    move ie polyfill import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import 'react-app-polyfill/ie9';
 import logo from './logo.svg';
 import './App.css';
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'react-app-polyfill/ie9';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
